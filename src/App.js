60c8fac6befbdc25d889c0b9882e959036f30349
Marta <martalauza@inbox.lv>
2022-09-17 14:43:39 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was created by Marta Lauza and is{" "}
-        <a
-          href="https://github.com/martalauza/react-weather-app"
-          target="blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was created by Marta Lauza and is{" "}
+          <a
+            href="https://github.com/martalauza/react-weather-app"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
